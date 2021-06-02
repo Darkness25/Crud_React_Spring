c83fa1212409a11890716b09b60574bb5f347384
JesusL <thedarkness2555@gmail.com>
2021-06-01 20:48:07 -0500

    Creamos las funciones dentro del servicio tales como save(), delete(), etc Inyectamos nuestro service en el controlador Creamos las funciones del controlador, y realizamos las anotaciones con @GetMapping, @PostMapping, @DeleteMapping, @PutMapping en las respectivas funciones

--- demo/src/main/java/com/crudJesusLara/demo/TodoService.java
+++ demo/src/main/java/com/crudJesusLara/demo/TodoService.java
@@ -11,7 +11,16 @@ public class TodoService {
 
     public Iterable<Todo> list(){
         return repository.findAll();
-
     }
 
+    public Todo save(Todo todo) {
+        return repository.save(todo);
+    }
+    public void delete (Long id){
+        repository.delete(get(id));
+    }
+
+    public Todo get(Long id){
+       return repository.findById(id).orElseThrow();
+    }
 }
